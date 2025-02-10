8904047d23b21079afe2fa51acef0ae57e595818
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-02-10 16:19:03 +0530

    setting the routes

---
+++ src/components/Advance.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Advance = () => {
+  return <h2>Advanced DSA Content</h2>;
+};
+
+export default Advance;
---
+++ src/components/Intermediate.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Intermediate = () => {
+  return <h2>Intermediate DSA Content</h2>;
+};
+
+export default Intermediate;
